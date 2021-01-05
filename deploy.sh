ad8d6542b66cca603bb7bc30918e5804d3fafb92
BoBoooooo <17746714@qq.com>
2021-01-05 13:42:06 +0800

    chore: 修改deploy sh 路径

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:boboooooo/Element-Pro-Crud-Doc.git master:gh-pages
+git push -f git@github.com:BoBoooooo/Element-Pro-Crud-Doc.git master:gh-pages
 
 cd -
